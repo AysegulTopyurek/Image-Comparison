d4a246bc0875f035032632d31fa30cdbd1c14dbe
aysegu1 <aysegultopyurek@outlook.com>
2019-03-11 19:31:18 +0300

    first commit

---
+++ WindowsFormsApp11/Form1.Designer.cs
@@ -0,0 +1,347 @@
+﻿namespace WindowsFormsApp11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button16 = new System.Windows.Forms.Button();
+            this.button15 = new System.Windows.Forms.Button();
+            this.button14 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button17 = new System.Windows.Forms.Button();
+            this.button18 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button16);
+            this.panel1.Controls.Add(this.button15);
+            this.panel1.Controls.Add(this.button14);
+            this.panel1.Controls.Add(this.button13);
+            this.panel1.Controls.Add(this.button12);
+            this.panel1.Controls.Add(this.button11);
+            this.panel1.Controls.Add(this.button10);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(3, 25);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 413);
+            this.panel1.TabIndex = 0;
+            // 
+            // button16
+            // 
+            this.button16.Location = new System.Drawing.Point(338, 330);
+            this.button16.Name = "button16";
+            this.button16.Size = new System.Drawing.Size(100, 100);
+            this.button16.TabIndex = 15;
+            this.button16.Text = "button16";
+            this.button16.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            this.button15.Location = new System.Drawing.Point(232, 330);
+            this.button15.Name = "button15";
+            this.button15.Size = new System.Drawing.Size(100, 100);
+            this.button15.TabIndex = 14;
+            this.button15.Text = "button15";
+            this.button15.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            this.button14.Location = new System.Drawing.Point(126, 330);
+            this.button14.Name = "button14";
+            this.button14.Size = new System.Drawing.Size(100, 100);
+            this.button14.TabIndex = 13;
+            this.button14.Text = "button14";
+            this.button14.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(20, 330);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(100, 100);
+            this.button13.TabIndex = 12;
+            this.button13.Text = "button13";
+            this.button13.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(338, 224);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(100, 100);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "button12";
+            this.button12.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(232, 224);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(100, 100);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "button11";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(126, 224);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(100, 100);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "button10";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(20, 224);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 100);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(338, 118);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(100, 100);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(232, 118);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 100);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(126, 118);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 100);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(20, 118);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 100);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(338, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 100);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(232, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(100, 100);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(126, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 100);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(20, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 100);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(482, 74);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(137, 26);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(478, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "High Score";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(647, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Your Score";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(651, 74);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(137, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button17
+            // 
+            this.button17.Location = new System.Drawing.Point(505, 106);
+            this.button17.Name = "button17";
+            this.button17.Size = new System.Drawing.Size(114, 41);
+            this.button17.TabIndex = 16;
+            this.button17.Text = "Mix";
+            this.button17.UseVisualStyleBackColor = true;
+            this.button17.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // button18
+            // 
+            this.button18.Location = new System.Drawing.Point(638, 106);
+            this.button18.Name = "button18";
+            this.button18.Size = new System.Drawing.Size(150, 41);
+            this.button18.TabIndex = 17;
+            this.button18.Text = "Choose image";
+            this.button18.UseVisualStyleBackColor = true;
+            this.button18.Click += new System.EventHandler(this.button18_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(459, 237);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(342, 201);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 18;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button18);
+            this.Controls.Add(this.button17);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button16;
+        private System.Windows.Forms.Button button15;
+        private System.Windows.Forms.Button button14;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button17;
+        private System.Windows.Forms.Button button18;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WindowsFormsApp11/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;
+
+namespace WindowsFormsApp11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Image img;
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+
+
+
+
+
+            OpenFileDialog openDialog = new OpenFileDialog();
+            if (openDialog.ShowDialog() == DialogResult.OK)
+            {
+                img = new Bitmap(openDialog.FileName);
+
+
+
+            }
+
+
+
+            int widthThird = (int)((double)img.Width / 4.0 + 0.5);
+            int heightThird = (int)((double)img.Height / 4.0 + 0.5);
+            //var img = Image.FromFile("media\\a.png");
+            Bitmap[,] bmps = new Bitmap[4, 4];
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    var index = i * 4 + j;
+
+                    bmps[i, j] = new Bitmap(widthThird, heightThird);
+                    Graphics g = Graphics.FromImage(bmps[i, j]);
+                    g.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle(j * widthThird, i * heightThird, widthThird, heightThird), GraphicsUnit.Pixel);
+                    g.Dispose();
+
+                }
+            }
+            button1.Image = bmps[0, 0];
+            button2.Image = bmps[0, 1];
+            button3.Image = bmps[0, 2];
+            button4.Image = bmps[0, 3];
+            button5.Image = bmps[1, 0];
+            button6.Image = bmps[1, 1];
+            button7.Image = bmps[1, 2];
+            button8.Image = bmps[1, 3];
+            button9.Image = bmps[2, 0];
+            button10.Image = bmps[2, 1];
+            button11.Image = bmps[2, 2];
+            button12.Image = bmps[2, 3];
+            button13.Image = bmps[3, 0];
+            button14.Image = bmps[3, 1];
+            button15.Image = bmps[3, 2];
+            button16.Image = bmps[3, 3];
+
+
+
+
+
+
+
+        }
+        Random r;
+        List<Button> buttons;
+        private void Whack_A_Mole_Load(object sender, EventArgs e)
+        {
+            r = new Random();
+
+            buttons = new List<Button>
+            {
+             button1,
+            button2,
+            button3,
+            button4,
+            button5,
+            button6,
+            button7,
+            button8,
+            button9,
+            button10,
+            button11,
+            button12,
+            button13,
+            button14,
+            button15,
+            button16,
+        };
+            foreach (Button button in buttons)
+                button.Visible = false;
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+
+            var randomValue = r.Next(0, 16);
+            Button btn = this.Controls.Find("button" + randomValue, true).FirstOrDefault() as Button;
+            btn.Visible = true;
+
+        }
+    }
+}
+
+            /*
+            //Random rand = new Random();
+            int rows = 5;//No of Rows as per Desire
+            int columns = 6;//No of columns as per Desire
+            var imgarray = new Image[rows, columns];//Create Image Array of Size Rows X Colums
+            var img = image1;//Get Image from anywhere, From File Or Using Dialogbox used previously
+            int height = img.Height;
+            int width = img.Width;//Get image Height & Width of Input Image
+            int one_img_h = height / rows;
+            int one_img_w = width / columns;//You need Rows x Columns, So get 1/rows Height, 1/columns width of original Image
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < columns; j++)
+                {
+                    imgarray[i, j] = new Bitmap(one_img_w, one_img_h);//generating new bitmap
+                    var graphics = Graphics.FromImage(imgarray[i, j]);
+                    graphics.DrawImage(img, new Rectangle(0, 0, one_img_w, one_img_h), new Rectangle(j * one_img_w, i * one_img_h, one_img_w, one_img_h), GraphicsUnit.Pixel);//Generating Splitted Pieces of Image
+                    graphics.Dispose();
+                }
+            }
+            //Image Is spitted You can use it by getting image from **imgarray[Rows, Columns]**
+            //Or You can Save it by using Following Code
+
+
+            var destinationFolderName = "";//Define a saving path
+            FolderBrowserDialog FolderBrowserDialog1 = new FolderBrowserDialog();
+            DialogResult result = FolderBrowserDialog1.ShowDialog();//Get folder Path Where splitted Image Saved
+            if (result == DialogResult.OK)
+            {
+                destinationFolderName = FolderBrowserDialog1.SelectedPath;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < columns; j++)
+                    {
+                        imgarray[i, j].Save(@"" + destinationFolderName + "/Image_" + i + "_" + j + ".jpg");//Save every image in Array [row][column] on local Path
+                    }
+                }
+            }
+        } 
+       /* Bitmap bmp1;
+        ArrayList arr = new ArrayList();
+        ArrayList arr2 = new ArrayList();
+
+       
+            */
+            /*  openFileDialog1.ShowDialog();
+
+              var img = Image.FromFile(openFileDialog1.FileName);
+              var imgarray = new Image[16];
+              int widthThird = (int)((double)img.Width / 4.0 + 0.5);
+              int heightThird = (int)((double)img.Height / 4.0 + 0.5);
+
+              for (int i = 0; i < 4; i++)
+              {
+                  for (int j = 0; j < 4; j++)
+                  {
+                      var index = i * 4 + j;
+                      imgarray[index] = new Bitmap(widthThird, heightThird);
+                      var graphics = Graphics.FromImage(imgarray[index]);
+                      graphics.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle(j * widthThird, i * heightThird, widthThird, heightThird), GraphicsUnit.Pixel);
+                      graphics.Dispose();
+                  }
+                  button1.Image = imgarray[0];
+                  button2.Image = imgarray[1];
+                  button3.Image = imgarray[2];
+                  button4.Image = imgarray[3];
+                  button5.Image = imgarray[4];
+                  button6.Image = imgarray[5];
+                  button7.Image = imgarray[6];
+                  button8.Image = imgarray[7];
+                  button9.Image = imgarray[8];
+                  button10.Image = imgarray[9];
+                  button11.Image = imgarray[10];
+                  button12.Image = imgarray[11];
+                  button13.Image = imgarray[12];
+                  button14.Image = imgarray[13];
+                  button15.Image = imgarray[14];
+                  button16.Image = imgarray[15];
+
+              }*/// a.png has 312X312 width and height
+                 /*int widthThird = (int)((double)img.Width / 4.0 + 0.5);
+                 int heightThird = (int)((double)img.Height / 4.0 + 0.5);
+                 Bitmap[,] bmps = new Bitmap[4, 4];
+                 for (int i = 0; i < 4; i++)
+                     for (int j = 0; j < 4; j++)
+                     {
+                         bmps[i, j] = new Bitmap(widthThird, heightThird);
+                         Graphics g = Graphics.FromImage(bmps[i, j]);
+                         g.DrawImage(img, new Rectangle(0, 0, widthThird, heightThird), new Rectangle(j * widthThird, i * heightThird, widthThird, heightThird), GraphicsUnit.Pixel);
+                         g.Dispose();
+                     }
+                 button1.Image = bmps[0, 0];
+                 button2.Image = bmps[0, 1];
+                 button3.Image = bmps[0, 2];
+                 button4.Image = bmps[0, 3];
+                 button5.Image = bmps[1, 0];
+                 button6.Image = bmps[1, 1];
+                 button7.Image = bmps[1, 2];
+                 button8.Image = bmps[1, 3];
+                 button9.Image = bmps[2, 0];
+                 button10.Image = bmps[2, 1];
+                 button11.Image = bmps[2, 2];
+                 button12.Image = bmps[2, 3];
+                 button13.Image = bmps[3, 0];
+                 button14.Image = bmps[3, 1];
+                 button15.Image = bmps[3, 2];
+                 button16.Image = bmps[3, 3];
+             }*/
+
+            /*
+            arr.Clear();
+            openFileDialog1.ShowDialog();
+            bmp1 = (Bitmap)Bitmap.FromFile(openFileDialog1.FileName);
+            int kare = 160;
+            for (int i = 0; i < bmp1.Width; i++)
+            {
+                for (int k = 0; k < bmp1.Height; k++)
+                {
+                    arr.Add(bmp1.GetPixel(i, k).Name);
+                }
+                if (bmp1.Height % kare != 0 || bmp1.Width % kare != 0)
+                {
+                    string gecerliOlculer = OrtakKatlar(bmp1.Height, bmp1.Width);
+
+                    return;
+                }
+                int dikeyParca = bmp1.Height / kare;
+                int yatayParca = bmp1.Width / kare;
+                Rectangle cropAlani = new Rectangle(0, 0, kare, kare);
+
+                Directory.CreateDirectory(@"C:\Users\user\Desktop\PROJELER");
+
+                for (int d = 0; d < dikeyParca; d++)
+                {
+                    for (int y = 0; y < yatayParca; y++)
+                    {
+                        cropAlani.Y = d * kare;
+                        cropAlani.X = y * kare;
+                        Image parcaResim = Crop(bmp1, cropAlani);
+                        parcaResim.Save(@"C:\Users\user\Desktop\PROJELER\" + d + "x" + y + ".jpg", ImageFormat.Jpeg);
+                    }
+                }
+
+
+            }
+        }
+
+
+            private static Image Crop(Image image, Rectangle rect)
+            {
+                Bitmap resim = new Bitmap(image);
+                Bitmap parcaResim = resim.Clone(rect, resim.PixelFormat);
+
+                return (Image)(parcaResim);
+            }
+
+        
+
+            private static string OrtakKatlar(int sayi1, int sayi2)
+            {
+                int kucukSayi = Math.Min(sayi1, sayi2);
+                string ortak = "";
+
+                for (int i = 2; i <= kucukSayi; i++)
+                {
+                    if (sayi1 % i == 0 && sayi2 % i == 0)
+                        ortak += i.ToString() + ",";
+                }
+
+                ortak = ortak.Remove(ortak.Length - 1);
+
+                return ortak;
+            }*/
+
